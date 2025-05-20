7af485d2c124afae55dbdf962cf586fd21d3fb3b
ArchitAvd <archit.avadhani@gmail.com>
2025-05-20 19:26:00 +0530

    added account screen template

--- src/screens/Account.tsx
+++ src/screens/Account.tsx
@@ -1,7 +1,8 @@
 import { useTheme } from '@react-navigation/native';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Header from '~/components/Header';
+import { FontAwesome5 } from '@expo/vector-icons';
 
 export default function Account() {
   const { colors } = useTheme();
@@ -13,6 +14,21 @@ export default function Account() {
       <View className="flex-row">
         <Header />
       </View>
+      <ScrollView
+        contentContainerStyle={{ paddingBottom: 20 }}
+        showsVerticalScrollIndicator={false}>
+        <View className="mt-5 flex-row justify-between px-4">
+          <Text style={{ color: colors.text, fontSize: 24 }}>Name</Text>
+          <FontAwesome5 name="bell" size={24} color={colors.text} />
+        </View>
+
+        <View className="mx-4 mt-4 rounded-lg p-4" style={{ backgroundColor: colors.card }}>
+          <Text className="mb-2 uppercase" style={{ color: colors.text, fontSize: 18 }}>
+            Username
+          </Text>
+          <Text className="text-gray-600">email id</Text>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
